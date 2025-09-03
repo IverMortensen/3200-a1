185f28d2ed55b6c8327111c8c561fee0adfc57a6
Iver Mortensen <iver.mortensen@gmail.com>
2025-09-03 12:22:46 +0200

    Updated run.sh

--- run.sh
+++ run.sh
@@ -6,49 +6,68 @@ if [ $# -eq 0 ]; then
 fi
 
 num_nodes=$1
-target="$HOME/3200-a1/web-server/target/release/web-server"
+target="./web-server"
 active_nodes_file="active_nodes.txt"
 stdout_dir="./stdout"
 mkdir -p "$stdout_dir"
 
+all_nodes=()
+nodes=()
 ports=()
 
 # Get available nodes
-all_nodes=$(/share/ifi/available-nodes.sh)
-readarray -t selected_nodes < <(echo "$all_nodes" | shuf -n "$num_nodes")
+readarray -t all_nodes < <(/share/ifi/available-nodes.sh)
+num_available_nodes=${#all_nodes[@]}
 
-# Store active nodes on disk for clean up script
-printf "%s\n" "${selected_nodes[@]}" >> "$active_nodes_file"
+if [ $num_available_nodes -eq 0 ]; then
+    echo "No nodes available."
+    exit 1
+fi
 
-echo "Selected nodes:"
-printf "%s\n" "${selected_nodes[@]}"
-echo ""
+# Select random nodes
+if [ $num_available_nodes -ge $num_nodes ]; then
+    readarray -t nodes < <(printf '%s\n' "${all_nodes[@]}" | shuf -n "$num_nodes")
+else
+    # Extend nodes with repeats if not enough nodes
+    readarray -t nodes < <(printf '%s\n' "${all_nodes[@]}" | shuf)
+    missing_nodes=$((num_nodes - num_available_nodes))
+    index=0
+    while [ $missing_nodes -gt 0 ]; do
+        nodes+=(${nodes[((index % num_available_nodes))]})
+        ((missing_nodes--))
+        ((index++))
+    done
+fi
+
+# Store name of active nodes on disk for clean up script
+printf "%s\n" "${nodes[@]}" | sort -u >> "$active_nodes_file"
 
 # Start processes
-for node in "${selected_nodes[@]}"; do
-    if [ -n "$node" ]; then
-        echo "Processing node: $node"
-        port=$(shuf -i 49152-65535 -n1)
-        ports+=("$port")
-        ssh "$USER"@"$node" "$target" "$node" "$port" > "${stdout_dir}/${node}" 2>&1 &
-    fi
-done
+for i in "${!nodes[@]}"; do
+    node="${nodes[$i]}"
+    echo "Processing node: $node"
+    port=$(shuf -i 49152-65535 -n1)
+    ports+=("$port")
 
+    # Copy target file to node (one for each proccess)
+    # and start the proccess
+    temp_file="/tmp/web-server-$$-$i"
+    {
+        scp "$target" "$USER@$node:$temp_file" && \
+        ssh "$USER@$node" "chmod +x $temp_file && $temp_file $node $port; rm -f $temp_file"
+    } > "${stdout_dir}/${node}" 2>&1 &
+done
 echo ""
 
 # Create and print the json output
 json_output="["
-index=0
-for node in "${selected_nodes[@]}"; do
-    if [ -n "$node" ]; then
-        if [ "$index" = 0 ]; then
-            json_output="${json_output}\"${node}:${ports["$index"]}\""
-        else
-            json_output="${json_output}, \"${node}:${ports["$index"]}\""
-        fi
-        ((index++))
+for i in "${!nodes[@]}"; do
+    node="${nodes[$i]}"
+    if [ "$i" = 0 ]; then
+        json_output="${json_output}\"${node}:${ports[$i]}\""
+    else
+        json_output="${json_output}, \"${node}:${ports[$i]}\""
     fi
 done
 json_output="${json_output}]"
-
 echo "$json_output"
